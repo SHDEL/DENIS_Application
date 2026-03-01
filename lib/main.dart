c290faea2c105138c21fcac70201fedb9e3cdb0c
Deler <del2547.pv@gmail.com>
2026-03-01 16:56:06 +0700

    Add Firebase configuration and dependencies add WidgetBinding in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:DENIS/firebase_options.dart';
 import 'package:DENIS/presentation/pages/splash.dart';
 import 'package:DENIS/presentation/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   runApp(const MyApp());
 }
 
