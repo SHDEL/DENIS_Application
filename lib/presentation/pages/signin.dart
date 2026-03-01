babc6eb10a04c1b5b0cc297d703f1d64ad4c2e9a
Deler <del2547.pv@gmail.com>
2026-03-01 16:10:19 +0700

    add Sign In Page

--- lib/presentation/pages/signin.dart
+++ lib/presentation/pages/signin.dart
@@ -1,27 +1,26 @@
 import 'package:DENIS/presentation/pages/homepage.dart';
-import 'package:DENIS/presentation/pages/signin.dart';
+import 'package:DENIS/presentation/pages/signup.dart';
 import 'package:DENIS/presentation/theme/app_theme.dart';
-
 import 'package:DENIS/presentation/widgets/primary_app_button.dart';
 import 'package:DENIS/presentation/widgets/primary_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SignUpPage extends StatefulWidget {
-  const SignUpPage({super.key});
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
 
   @override
-  State<SignUpPage> createState() => _SignUpPageState();
+  State<SignInPage> createState() => _SignInPageState();
 }
 
-class _SignUpPageState extends State<SignUpPage> {
+class _SignInPageState extends State<SignInPage> {
 
-  VoidCallback? get onPressedSignUp => () {
+  VoidCallback? get onPressedSignIn => () {
     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Home')));
   };
 
-  VoidCallback? get onPressedSignIn => () {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SignInPage()));
+  VoidCallback? get onPressedSignUp => () {
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SignUpPage()));
   };
 
   @override
@@ -40,21 +39,19 @@ class _SignUpPageState extends State<SignUpPage> {
                   const SizedBox(height: 44),
                   SvgPicture.asset('assets/image/DenisTextIcon.svg'),
                   const SizedBox(height: 44),
-                  Text('Sign Up', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: AppTheme.lightTheme.primaryColor)),
+                  Text('Sign In', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: AppTheme.lightTheme.primaryColor)),
                   const SizedBox(height: 24),
-                  PrimaryInput(text: 'Username'),
-                  const SizedBox(height: 24),
-                  PrimaryInput(text: 'Email'),
+                  PrimaryInput(text: 'Username or Email'),
                   const SizedBox(height: 24),
                   PrimaryInput(text: 'Password'),
                   const SizedBox(height: 44),
-                  PrimaryAppButton(onPressed: onPressedSignUp, text: 'Create Account'),
+                  PrimaryAppButton(onPressed: onPressedSignIn, text: 'Sign In'),
                   const SizedBox(height: 24),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Already have an account?', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 14)),
-                      TextButton(onPressed: onPressedSignIn, child: Text('Sign In', style: TextStyle(color: AppTheme.lightTheme.primaryColor, fontSize: 14))),
+                      Text('Don\'t have an account?', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 14)),
+                      TextButton(onPressed: onPressedSignUp, child: Text('Sign Up', style: TextStyle(color: AppTheme.lightTheme.primaryColor, fontSize: 14))),
                     ],
                   ),
 
