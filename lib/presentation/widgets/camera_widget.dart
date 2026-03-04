44561add4386a01045e96e1beb920c284129794a
Deler <del2547.pv@gmail.com>
2026-03-05 00:53:59 +0700

    add camera widget

---
+++ lib/presentation/widgets/camera_widget.dart
@@ -0,0 +1,58 @@
+
+import 'package:DENIS/presentation/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widget_previews.dart';
+
+class CameraWidget extends StatefulWidget {
+  const CameraWidget({super.key});
+
+  @override
+  State<CameraWidget> createState() => _CameraWidgetState();
+}
+
+class _CameraWidgetState extends State<CameraWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+            decoration: BoxDecoration(
+              gradient: AppColors.gradprimary,
+              borderRadius: BorderRadius.circular(28),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.20),
+                  blurRadius: 24,
+                  spreadRadius: 0,
+                  offset: Offset(0, 1),
+                )
+              ]
+            ),
+            child: const Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.videocam,
+                    color: Colors.white54,
+                    size: 56,
+                  ),
+                  SizedBox(height: 10),
+                  Text('Camera',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 20,
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+  }
+}
+
+@Preview(name: 'Camera Widget')
+Widget cameraWidgetPreview() {
+  return const CameraWidget();
+}
